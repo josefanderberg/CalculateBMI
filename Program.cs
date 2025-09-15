9f8334c82d02b3f939588ec49dd1b4ea5b470163
Josef Anderberg <josef.anderberg@gmail.com>
2025-09-15 14:48:38 +0200

    Made the basics

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Example usage of the new C# 13.0 feature: Primary Constructors for non-record classes
+        if (uint == "metric")
+        {
+            return weight / (height * height);
+        }
+        else if (uint == "imperial")
+        {
+            return 703 * (weight / (height * height))
+        }
+    }
+}
